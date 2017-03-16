f5fcb48ae23d86690fc61048f4d00f03e154def5
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-03-16 18:09:39 +0800

    Change SendSMSJob class to read properties from cmd injection

--- send-sms-ftp-engine/src/main/java/com/bcm/app/engine/SendSMSJob.java
+++ send-sms-ftp-engine/src/main/java/com/bcm/app/engine/SendSMSJob.java
@@ -2,16 +2,18 @@ package com.bcm.app.engine;
 
 import org.apache.log4j.Logger;
 
-import java.io.File;
+import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Properties;
+import java.lang.*; 
 
 import com.bcm.app.core.*;
 
 public class SendSMSJob extends Thread {
 
     final static Logger mLogger = Logger.getLogger(SendSMSJob.class);
-
+/*
     private final String FTP_ADDRESS = "172.18.255.108";
     private final int FTP_PORT = 21;
     private final String FTP_USER = "itoper";
@@ -25,10 +27,49 @@ public class SendSMSJob extends Thread {
     private final String OUTMSG = "outmsg.txt";
     
     private final List<FileManipulator> mProcessChain = new ArrayList<FileManipulator>();
-    
+*/
+  
     private boolean mIsActive;
+    private List<FileManipulator> mProcessChain = new ArrayList<FileManipulator>();
     
-    public SendSMSJob() {
+    private String FTP_ADDRESS;
+    private int FTP_PORT;
+    private String FTP_USER;
+    private String FTP_PASSWORD;
+    private String FTP_FOLDER;
+    private String SMS_FILE;
+    private String SMS_FOLDER;
+    private String BKUP_FOLDER;
+    private String CUR_DIR;
+    private String INMSG;
+    private String OUTMSG;
+
+    public SendSMSJob(String propertiesPath) {
+        
+        super();
+        
+        /* Get properties */
+        Properties prop = new Properties();
+        InputStream input = null;
+
+        try {
+            input = new FileInputStream(propertiesPath);
+            prop.load(input);
+            FTP_ADDRESS = prop.getProperty("FTP_ADDRESS");
+            FTP_PORT = Integer.parseInt(prop.getProperty("FTP_PORT"));
+            FTP_USER = prop.getProperty("FTP_USER");
+            FTP_PASSWORD = prop.getProperty("FTP_PASSWORD");
+            FTP_FOLDER = prop.getProperty("FTP_FOLDER");
+            SMS_FILE = prop.getProperty("SMS_FILE");
+            SMS_FOLDER = prop.getProperty("SMS_FOLDER");
+            BKUP_FOLDER = prop.getProperty("BKUP_FOLDER");
+            CUR_DIR = System.getProperty("user.dir");
+            INMSG = prop.getProperty("INMSG");
+            OUTMSG = prop.getProperty("OUTMSG");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
         /* Set up MessageRegisterLogProxy */
         MessageRegisterLogProxy messageRegisterLogProxy = new MessageRegisterLogProxy();
         
@@ -90,7 +131,7 @@ public class SendSMSJob extends Thread {
     }
     
     public static void main(String[] args) {
-        SendSMSJob job = new SendSMSJob();
+        SendSMSJob job = new SendSMSJob(args[0]);
         job.logTheStart();
         job.run(); //not using thread, start using the current thread, terminated by Ctrl C in bash
     }
