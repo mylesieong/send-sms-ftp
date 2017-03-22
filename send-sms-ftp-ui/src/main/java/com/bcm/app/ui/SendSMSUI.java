9c36ec397120ef3b423579e14e652f293b11c0e3
Myles Ieong Sai Hou <BI77@corp.dahsing.com>
2017-03-22 11:30:03 +0800

    Add load config screen basic

--- send-sms-ftp-ui/src/main/java/com/bcm/app/ui/SendSMSUI.java
+++ send-sms-ftp-ui/src/main/java/com/bcm/app/ui/SendSMSUI.java
@@ -16,7 +16,9 @@ import com.bcm.app.engine.SendSMSJob;
 public class SendSMSUI implements ActionListener{
 
     private SendSMSJob mJob;
-    private JFrame mFrame;
+    
+    /* Main Frame properties */
+    private JFrame mMainFrame;
     private JLabel mLastSentTimeTagLabel;
     private JLabel mLastSentTimeLabel;
     private JLabel mJobStatusTagLabel;
@@ -26,8 +28,22 @@ public class SendSMSUI implements ActionListener{
     private JButton mStopButton;
     private JButton mExportLogButton;
     private JButton mClearLogButton;
-    private JButton mChangeConfigButton;
+    private JButton mLoadConfigButton;
     private JButton mExitButton;
+    
+    /* Load Config Frame properties */
+    private JFrame mLoadConfigFrame;
+    private JLabel mFtpAddressTagLabel;
+    private JLabel mFtpAddressLabel;
+    // private JLabel mFtpPortTagLabel;
+    // private JLabel mFtpPortLabel;
+    // private JLabel mFtpUserTagLabel;
+    // private JLabel mFtpUserLabel;
+    // private JLabel mFtpPasswrodTagLabel;
+    // private JLabel mFtpPasswordLabel;
+    // more label to be added...
+    private JButton mLoadConfirmButton;
+    private JButton mLoadCancelButton;
 
     /**
      * Launch the application.
@@ -37,7 +53,7 @@ public class SendSMSUI implements ActionListener{
             public void run() {
                 try {
                     SendSMSUI window = new SendSMSUI();
-                    window.mFrame.setVisible(true);
+                    window.mMainFrame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -48,77 +64,108 @@ public class SendSMSUI implements ActionListener{
     /**
      * Create the application.
      */
-    public SendSMSUI() {
+    public SendSMSUI() {          
+    
+        mJob = new SendSMSJob();
+        mJob.setProperties("config.properties");
+        
         initialize();
     }
 
     /**
-     * Initialize the contents of the mFrame.
+     * Initialize all Frames.
      */
-    private void initialize() {
+    private void initialize (){
+        initializeMain();
+        initializeLoadConfig();
+    }
         
-        mJob = new SendSMSJob();
-        mJob.setProperties("config.properties");
-        
-        mFrame = new JFrame();
-        mFrame.setBounds(100, 100, 360, 360);
-        mFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        mFrame.getContentPane().setLayout(null);
+    /**
+     * Init mMainFrame
+     */
+    private void initializeMain() {
+
+        mMainFrame = new JFrame();
+        mMainFrame.setBounds(100, 100, 360, 360);
+        mMainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mMainFrame.getContentPane().setLayout(null);
         
         /*Last sent time */
         mLastSentTimeTagLabel = new JLabel("Last Sent Time: ");
         mLastSentTimeTagLabel.setBounds(26, 32, 100, 16);
-        mFrame.getContentPane().add(mLastSentTimeTagLabel);
+        mMainFrame.getContentPane().add(mLastSentTimeTagLabel);
         
         mLastSentTimeLabel = new JLabel("[...]");
         mLastSentTimeLabel.setBounds(136, 33, 175, 14);
-        mFrame.getContentPane().add(mLastSentTimeLabel);
+        mMainFrame.getContentPane().add(mLastSentTimeLabel);
         
         /* Job status */
         mJobStatusTagLabel = new JLabel("Job Status: ");
         mJobStatusTagLabel.setBounds(26, 59, 100, 14);
-        mFrame.getContentPane().add(mJobStatusTagLabel);
+        mMainFrame.getContentPane().add(mJobStatusTagLabel);
         
         mJobStatusLabel = new JLabel("[...]");
         mJobStatusLabel.setBounds(136, 59, 175, 14);
-        mFrame.getContentPane().add(mJobStatusLabel);
+        mMainFrame.getContentPane().add(mJobStatusLabel);
         
         /* Moment status */
         mMomentLabel = new JLabel("MOMENT");
         mMomentLabel.setHorizontalAlignment(SwingConstants.CENTER);
         mMomentLabel.setFont(new Font("Tahoma", Font.PLAIN, 25));
         mMomentLabel.setBounds(0, 84, 344, 48);
-        mFrame.getContentPane().add(mMomentLabel);
+        mMainFrame.getContentPane().add(mMomentLabel);
         
         /* Button: Start and stop */
         mStartButton = new JButton("Start Send SMS");
         mStartButton.setBounds(26, 154, 125, 25);
         mStartButton.addActionListener(this);
-        mFrame.getContentPane().add(mStartButton);
+        mMainFrame.getContentPane().add(mStartButton);
         
         mStopButton = new JButton("Stop Send SMS");
         mStopButton.setBounds(186, 154, 125, 25);
         mStopButton.addActionListener(this);
-        mFrame.getContentPane().add(mStopButton);
+        mMainFrame.getContentPane().add(mStopButton);
         
         /* Button: log export and log clear */
         mExportLogButton = new JButton("Export Log File");
         mExportLogButton.setBounds(26, 188, 125, 25);
-        mFrame.getContentPane().add(mExportLogButton);
+        mMainFrame.getContentPane().add(mExportLogButton);
         
         mClearLogButton = new JButton("Clear Log File");
         mClearLogButton.setBounds(186, 188, 125, 25);
-        mFrame.getContentPane().add(mClearLogButton);
+        mMainFrame.getContentPane().add(mClearLogButton);
         
         /* Change config button*/
-        mChangeConfigButton = new JButton("Change Configuration");
-        mChangeConfigButton.setBounds(26, 224, 285, 25);
-        mFrame.getContentPane().add(mChangeConfigButton);
+        mLoadConfigButton = new JButton("Change Configuration");
+        mLoadConfigButton.setBounds(26, 224, 285, 25);
+        mLoadConfigButton.addActionListener(this);
+        mMainFrame.getContentPane().add(mLoadConfigButton);
         
         /* Exit button */
         mExitButton = new JButton("Exit");
         mExitButton.setBounds(26, 260, 285, 25);
-        mFrame.getContentPane().add(mExitButton);
+        mMainFrame.getContentPane().add(mExitButton);
+    }
+    
+    /**
+     * Init mMainFrame
+     */
+    private void initializeLoadConfig() {
+        mLoadConfigFrame = new JFrame();
+        mLoadConfigFrame.setBounds(100, 100, 360, 360);
+        mLoadConfigFrame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        mLoadConfigFrame.getContentPane().setLayout(null);
+        
+        /*Last sent time */
+        mFtpAddressTagLabel = new JLabel("Ftp Address: ");
+        mFtpAddressTagLabel.setBounds(26, 32, 100, 16);
+        mLoadConfigFrame.getContentPane().add(mFtpAddressTagLabel);
+        
+        /*Last sent time */
+        mFtpAddressLabel = new JLabel("[...] ");
+        mFtpAddressLabel.setBounds(136, 33, 175, 14);
+        mLoadConfigFrame.getContentPane().add(mFtpAddressLabel);
+        
     }
     
     @Override
@@ -133,6 +180,10 @@ public class SendSMSUI implements ActionListener{
         if (e.getSource() == this.mStopButton){
             this.mJob.setActive(false);
             this.mJobStatusLabel.setText("job ended.");
+        }
+                
+        if (e.getSource() == this.mLoadConfigButton){
+            this.mLoadConfigFrame.setVisible(true);
         }
     }
     
